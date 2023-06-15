6cca99fa788967d4ea4169a8dec43d2d5ce6bace
MasterJoker05 <mckevin.bautista@umak.edu.ph>
2023-06-15 21:15:14 +0800

    Update Products and Navbar

--- src/lib/module/page/products/products.svelte
+++ src/lib/module/page/products/products.svelte
@@ -1,5 +1,7 @@
 <script>
-    import { Card, CardHeader, CardBody } from '$lib/component/card';
+    import { Card, CardBody, CardImage} from '$lib/component/card';
+    import { Container, Row, Col } from '$lib/component/container';
+    import Header from '$lib/component/header/header.svelte';
     import StarRating from '$lib/module/page/products/starRating.svelte';
     let items = [
         {"productImage":"https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png", "productName":"Lorem ipsum dolor, sit amet consectetur adipisicing elit.","productPrice":"10.00","productRating":"5"},
@@ -16,33 +18,22 @@
         {"productImage":"https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png", "productName":"Lorem ipsum dolor, sit amet consectetur adipisicing elit.","productPrice":"66.00","productRating":"1"}
     ];
 </script>
-<div class="container">
-    <div class="row row-cols-2 row-cols-lg-6 g-2 g-lg-3">
+<Container>
+    <Row className={"row-cols-2 row-cols-lg-6 g-2 g-lg-3"}>
         {#each items as item}
-            <div class="col">
-                <!-- <a href="page.html" class="card p-0 text-decoration-none"> -->
-                    <Card classCard="p-0 text-decoration-none">
-                        <div class="rounded-top-2" style="background: url('{item.productImage}') no-repeat center center;
-                        background-size: cover;
-                        background-position: center;
-                        -webkit-background-size: cover;
-                        -moz-background-size: cover; 
-                        -o-background-size: cover;
-                        height: 170px;">
-                        </div>
-                        <CardBody>
-                            <h4 class="card-title h6 text-truncate fw-normal d-block">{item.productName}</h4>
-                            <h5 class="text-primary">Php {item.productPrice}</h5>
-                            <small>
-                                <StarRating rating={item.productRating}/>
-                                (200)
-                            </small>
-                        </CardBody>
-                    </Card>
-            </div>
+            <Col>
+                <Card className="p-0 text-decoration-none">
+                    <CardImage image="{item.productImage}"></CardImage>
+                    <CardBody>
+                        <Header header="h4" className={"card-title h6 text-truncate fw-normal d-block"}>{item.productName}</Header>
+                        <Header header="h5" className={"text-primary"}>Php {item.productPrice}</Header>
+                        <small>
+                            <StarRating rating={item.productRating}/>
+                            (200)
+                        </small>
+                    </CardBody>
+                </Card>
+            </Col>
         {/each}
-    </div>
-    <div class="text-center mt-3">
-      <button type="button" name="" id="" class="btn btn-outline-primary btn-lg rounded-0 w-50">Load More</button>
-    </div>
-</div>
+    </Row>
+</Container>
