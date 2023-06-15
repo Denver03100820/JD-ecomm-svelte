47fafa20af9bef9720e4166dcdff95e17f1d8747
MasterJoker05 <mckevin.bautista@umak.edu.ph>
2023-06-15 18:59:19 +0800

    initial Commit

---
+++ src/lib/markup/banner.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let BannerImage = [];
+</script>
+<section class="banner d-flex w-100">
+    <div id="carouselId" class="carousel slide w-100" data-bs-ride="carousel">
+    
+        <div class="carousel-inner" role="listbox">
+            each BannerImage 
+            <div class="carousel-item w-100 active">
+                <img src="https://lzd-img-global.slatic.net/g/icms/images/ims-web/be7d687d-9864-4117-bd5a-9f73d31a5ca4.jpg_2200x2200q90.jpg_.webp" class="w-100 h-50 d-block" alt="First slide">
+            </div>
+            <div class="carousel-item w-100">
+                <img src="https://lzd-img-global.slatic.net/g/icms/images/ims-web/be7d687d-9864-4117-bd5a-9f73d31a5ca4.jpg_2200x2200q90.jpg_.webp" class="w-100 h-50 d-block" alt="Second slide">
+            </div>
+            <div class="carousel-item w-100">
+                <img src="https://lzd-img-global.slatic.net/g/icms/images/ims-web/be7d687d-9864-4117-bd5a-9f73d31a5ca4.jpg_2200x2200q90.jpg_.webp" class="w-100 h-50 d-block" alt="Second slide">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <!-- <span class="visually-hidden">Previous</span> -->
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <!-- <span class="visually-hidden">Next</span> -->
+        </button>
+    </div>
+</section>
