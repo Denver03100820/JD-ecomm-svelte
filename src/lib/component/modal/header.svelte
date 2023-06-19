ba5d291acfe9ddc57529ac6a6c824d4a0c180998
John Denver Diaz <johndenver.diaz@umak.edu.ph>
2023-06-19 19:08:16 +0800

    login-register

--- src/lib/component/modal/header.svelte
+++ src/lib/component/modal/header.svelte
@@ -5,4 +5,5 @@
 
 <div class="modal-header {className}">
     <Header header="h5" className={"modal-title"} attributes={{"id":"modalTitleId"}}><slot></slot></Header>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
