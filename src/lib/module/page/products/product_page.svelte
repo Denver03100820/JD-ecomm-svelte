32d6d12e57f9c10c319476fbb3df1bf8bc123f23
solutionitc <devconsole@umak.edu.ph>
2023-06-19 20:57:11 +0800

    Product Page Link

---
+++ src/lib/module/page/products/product_page.svelte
@@ -0,0 +1,212 @@
+<header class="d-flex w-100 border-bottom py-2 px-2 px-md-4">
+    <a href="/"
+        class="nav-brand d-none flex-shrink-0 text-decoration-none d-flex ms-3 d-md-flex flex-column flex-md-row align-items-center"
+        style="width: 280px;">
+        <img src="shopping-online.png" width="50" alt="logo" />
+        <span class="lead fw-bolder ps-4 text-uppercase">LazShopee</span>
+    </a>
+    <a href="/"
+        class="nav-brand d-md-none flex-shrink-0 text-decoration-none d-flex flex-column flex-md-row align-items-center justify-content-center"
+        style="width: 100px;">
+        <img src="shopping-online.png" width="50" alt="logo" />
+    </a>
+    <nav class="navbar flex-grow-1">
+        <ul class="nav w-100 align-items-center">
+            <li class="nav-item">
+                <a class="nav-link " href="#"><i class="fa-solid fa-home"></i> &nbsp;Home</a>
+                
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-list"></i> &nbsp;Categories
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </li>
+            <li class="ms-auto nav-item">
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit"><i class="fa-solid fa-search"></i></button>
+                </form>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa-solid fa-sign-in"></i> Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="fa-solid fa-register"></i> Register</a>
+            </li>
+            <!-- <li class="nav-item">
+                <a href="#" class="nav-link dropdown-toggle show" type="button" data-bs-toggle="dropdown" aria-expanded="true">
+                    <img class="rounded-circle shadow-sm" src="https://bams.ortadeltech.com/assets/images/profile.png" width="50" alt="profile image link">
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end rounded-0 show" data-bs-popper="static">
+                    <li><a class="dropdown-item" href="#"><i class="fa fa-user px-2"></i> &nbsp; Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="http://localhost/bams"><i class="fa fa-power-off px-2"></i> &nbsp;Log-out</a></li>
+                </ul>
+            </li> -->
+            
+        </ul>
+    </nav>
+</header>
+<main class="d-flex flex-column flex-grow-1 overflow-y-auto">
+    <section class="content py-5 px-md-5" style="background: #f5f5f5;">
+        <nav class="breadcrumb mb-5">
+            <a class="breadcrumb-item text-decoration-none" href="#">Electronics</a>
+            <a class="breadcrumb-item text-decoration-none" href="#">Phones</a>
+            <span class="breadcrumb-item active" aria-current="page">Nokia</span>
+        </nav>
+        <div class="container">
+            <div class="product row bg-white p-4">
+                <div class="col-12 col-md-5 d-flex flex-column">
+                    <div class="rounded-2" style="background: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png') no-repeat center center;
+                    background-size: cover;
+                    background-position: center;
+                    -webkit-background-size: cover;
+                    -moz-background-size: cover; 
+                    -o-background-size: cover;
+                    height: 400px;">
+                    </div>
+                    <div class="d-flex justify-content-center mt-3">
+                        <div class="mx-3" style="background: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png') no-repeat center center;
+                        background-size: cover;
+                        background-position: center;
+                        -webkit-background-size: cover;
+                        -moz-background-size: cover; 
+                        -o-background-size: cover;
+                        height: 70px; width: 70px;">
+                        </div>
+                        <div class="mx-3" style="background: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png') no-repeat center center;
+                        background-size: cover;
+                        background-position: center;
+                        -webkit-background-size: cover;
+                        -moz-background-size: cover; 
+                        -o-background-size: cover;
+                        height: 70px; width: 70px;">
+                        </div>
+                        <div class="mx-3" style="background: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png') no-repeat center center;
+                        background-size: cover;
+                        background-position: center;
+                        -webkit-background-size: cover;
+                        -moz-background-size: cover; 
+                        -o-background-size: cover;
+                        height: 70px; width: 70px;">
+                        </div>
+                        <div class="mx-3" style="background: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png') no-repeat center center;
+                        background-size: cover;
+                        background-position: center;
+                        -webkit-background-size: cover;
+                        -moz-background-size: cover; 
+                        -o-background-size: cover;
+                        height: 70px; width: 70px;">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-7 d-flex flex-column px-5 py-3">
+                    <h3>Nokia</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur culpa voluptatem quibusdam accusantium ipsam asperiores hic dicta placeat eum minima nulla perspiciatis nemo, voluptatum libero, autem fugit aliquam error nesciunt?
+                    </p>
+                    <small>
+                        <i class="fa-solid fa-star text-warning"></i>
+                        <i class="fa-solid fa-star text-warning"></i>
+                        <i class="fa-solid fa-star text-warning"></i>
+                        <i class="fa-solid fa-star text-warning"></i>
+                        <i class="fa-solid fa-star text-warning"></i>
+                        (200)
+                    </small>
+                    <hr>
+                    
+
+                    <h4 class="h5">Options</h4>
+                    <div class="d-flex mt-1">
+                        <a href="" class="mx-3" style="background: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png') no-repeat center center;
+                        background-size: cover;
+                        background-position: center;
+                        -webkit-background-size: cover;
+                        -moz-background-size: cover; 
+                        -o-background-size: cover;
+                        height: 70px; width: 70px;">
+                        </a>
+                        <a href="" class="mx-3" style="background: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png') no-repeat center center;
+                        background-size: cover;
+                        background-position: center;
+                        -webkit-background-size: cover;
+                        -moz-background-size: cover; 
+                        -o-background-size: cover;
+                        height: 70px; width: 70px;">
+                        </a>
+                        <a href="" class="mx-3" style="background: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png') no-repeat center center;
+                        background-size: cover;
+                        background-position: center;
+                        -webkit-background-size: cover;
+                        -moz-background-size: cover; 
+                        -o-background-size: cover;
+                        height: 70px; width: 70px;">
+                        </a>
+                        <a href="" class="mx-3" style="background: url('https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png') no-repeat center center;
+                        background-size: cover;
+                        background-position: center;
+                        -webkit-background-size: cover;
+                        -moz-background-size: cover; 
+                        -o-background-size: cover;
+                        height: 70px; width: 70px;">
+                        </a>
+                    </div>
+                    <hr>
+                    <div class="d-flex align-items-center">
+                        Quantity
+                        <div class="input-group ms-3" style="width: 120px;">
+                            <button class="btn border border-end-0" type="button"><i class="fa-solid fa-minus text-success"></i></button>
+                            <input type="text" class="form-control border border-start-0 border-end-0 text-center text-success" value="0" aria-label="Example text with two button addons">
+                            <button class="btn border border-start-0" type="button"><i class="fa-solid fa-plus text-success"></i></button>
+                        </div>
+                        <small class="px-4" style="font-size: 11px;">
+                            Only <span class="text-danger fw-bolder">10 items</span> Left! <br>
+                            Don't miss it
+                        </small>
+                    </div>
+                    <div class="d-flex mt-4">
+                        <button class="btn btn-success text-center" style="width: 200px;">Buy Now</button>
+                        <button class="btn btn-outline-success text-center ms-3" style="width: 200px;">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+            <div class="description row bg-white p-4 mt-3">
+                <h4>Product Description</h4>
+                <div class="d-flex flex-column">
+                    Processor: <br>
+                    RAM: <br>
+                    Storage: 4 MB <br>
+                    Display: 2.4 inches <br>
+                    Battery: 1020 mAh <br>
+                    ModelNokia 125 <br>
+                    ReleasedMay 2020 <br>
+                    StatusAvailable <br>
+                    TypeBar <br>
+                    Dimensions132 x 50.5 x 15 mm <br>
+                    Weight91.3 Grams <br>
+                    Display TypeTFT <br>
+                    Size2.4 inches <br>
+                    Resolution240 x 320 pixels <br>
+                    Display Colors16M <br>
+                    Pixel Density167 ppi (pixels per inch) <br>
+                    Internal Storage4 MB <br>
+                    <img src="https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png" alt="" width="50%">
+                </div>
+            </div>
+            <div class="description row bg-white p-4 mt-3">
+                <h4>Ratings & Reviews</h4>
+                <div class="d-flex flex-column">
+                
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
--- src/lib/module/page/products/products.svelte
+++ src/lib/module/page/products/products.svelte
@@ -3,8 +3,11 @@
     import { Container, Row, Col } from '$lib/component/container';
     import Header from '$lib/component/header/header.svelte';
     import StarRating from '$lib/module/page/products/starRating.svelte';
+    import ItemPage from '$lib/module/page/products/product_page.svelte';
+   
+
     let items = [
-        {"productImage":"https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png", "productName":"Lorem ipsum dolor, sit amet consectetur adipisicing elit.","productPrice":"10.00","productRating":5},
+        {"productImage":"https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png", "productLink":'$lib/module/page/products/product_page.svelte', "productName":"Lorem ipsum dolor, sit amet consectetur adipisicing elit.","productPrice":"10.00","productRating":5},
         {"productImage":"https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png", "productName":"Lorem ipsum dolor, sit amet consectetur adipisicing elit.","productPrice":"20.00","productRating":4},
         {"productImage":"https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png", "productName":"Lorem ipsum dolor, sit amet consectetur adipisicing elit.","productPrice":"30.00","productRating":3},
         {"productImage":"https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png", "productName":"Lorem ipsum dolor, sit amet consectetur adipisicing elit.","productPrice":"40.00","productRating":2},
@@ -17,22 +20,34 @@
         {"productImage":"https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png", "productName":"Lorem ipsum dolor, sit amet consectetur adipisicing elit.","productPrice":"99.00","productRating":1},
         {"productImage":"https://www.stuff.tv/wp-content/uploads/sites/2/2022/11/Stuff-Best-Smartphone-Lead.png", "productName":"Lorem ipsum dolor, sit amet consectetur adipisicing elit.","productPrice":"66.00","productRating":1}
     ];
+
+
+
+
+
 </script>
 <Container>
     <Row className={"row-cols-2 row-cols-lg-6 row-cols-md-4 g-2 g-lg-3"}>
         {#each items as item}
             <Col>
-                <Card className="p-0 text-decoration-none">
-                    <CardImage image="{item.productImage}"></CardImage>
-                    <CardBody>
-                        <Header header="h4" className={"card-title h6 text-truncate fw-normal d-block"}>{item.productName}</Header>
-                        <Header header="h5" className={"text-primary"}>Php {item.productPrice}</Header>
-                        <Header header="small">
-                            <StarRating rating={item.productRating}/>
-                            ({item.productRating})
-                        </Header>
-                    </CardBody>
-                </Card>
+                
+                    <Card className="p-0 text-decoration-none">
+                        <a data-sveltekit-preload-data="tap" href="/lib/module/page/products/product_page.svelte">
+                        <CardImage image="{item.productImage}"></CardImage>
+                        </a>
+                        <CardBody>
+                            <a href={item.productLink}>
+                            <Header header="h4" className={"card-title h6 text-truncate fw-normal d-block"}>{item.productName}</Header>
+                            </a>
+                            <Header header="h5" className={"text-primary"}>Php {item.productPrice}</Header>
+                            <Header header="small">
+                                <StarRating rating={item.productRating}/>
+                                ({item.productRating})
+                            </Header>
+                        </CardBody>
+                    </Card>
+                
+                
             </Col>
         {/each}
     </Row>
